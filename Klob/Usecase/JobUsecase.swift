791b084e2400e4c4e503538c3767e40ad228d0f9
Nathan <nathancodelab@gmail.com>
2024-12-09 11:20:30 +0700

    feat: add job use case

---
+++ Klob/Usecase/JobUsecase.swift
@@ -0,0 +1,39 @@
+//
+//  JobUsecase.swift
+//  Klob
+//
+//  Created by Nathanael Abel on 09/12/24.
+//
+
+import Foundation
+
+class JobUsecase: ObservableObject {
+    private let BASE_URL = "https://test-server-klob.onrender.com/fakeJob/apple/academy"
+    
+    func getRecords() async throws -> [Job] {
+        guard let url = URL(string: BASE_URL) else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            // Check for HTTP errors
+            if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+                throw URLError(.badServerResponse)
+            }
+            
+            // Decode JSON response
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedResponse = try decoder.decode(JobList.self, from: data) // Decode to JobList (Array of Job)
+            return decodedResponse
+        } catch {
+            print("Error fetching data: \(error.localizedDescription)")
+            throw error
+        }
+    }
+}
