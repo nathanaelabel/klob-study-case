04e6c8d5638fd279598e2adbe4f2dac61b1dcb85
Nathan <nathancodelab@gmail.com>
2024-12-09 09:39:04 +0700

    Initial Commit

---
+++ Klob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Klob
+//
+//  Created by Nathanael Abel on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Klob/KlobApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlobApp.swift
+//  Klob
+//
+//  Created by Nathanael Abel on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KlobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
